84582faf8989a78b131bb40e7927cd7b6404eff2
Cláudio Araújo <claudioaraujo20@gmail.com>
2026-09-23 23:06:46 -0300

    feat(design): simplify not found dependencies

--- frontend/src/app/features/public/not-found/not-found.component.ts
+++ frontend/src/app/features/public/not-found/not-found.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [RouterLink, ButtonModule, TranslateModule],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
 })
 export class NotFoundComponent {}
